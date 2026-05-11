59647d1133025a2d81fd56f9a60d07096c6ec702
Adam Fay <adam.fay@vuw.ac.nz>
2026-05-11 16:30:25 +1200

    auto detect os

--- whiptail workstation select.sh
+++ whiptail workstation select.sh
@@ -1,31 +1,41 @@
-# !/bin/bash
-# This script provides a menu for selecting which workstation setup to install packages for.
+#!/bin/bash
+# Auto-detect Linux distro and run the correct workstation setup
+
 install=$(bash -c "curl -fsSL https://raw.githubusercontent.com/ScoobyChan/Linux-Scripts/refs/heads/main/pack_mgr_install.sh | sh | grep sudo")
 $install make
 
-whiptail --title "Workstation Setup" --menu "Select an option:" 15 50 4 \
-    "1" "Install Arch Linux packages" \
-    "2" "Install Debian packages" \
-    "3" "Install openSUSE packages" \
-    "4" "Install RHEL packages" \
-    "5" "Exit"
+# Detect OS
+if [[ -f /etc/os-release ]]; then
+    . /etc/os-release
+    DISTRO_ID=$ID
+    DISTRO_LIKE=$ID_LIKE
+else
+    echo "Cannot detect OS (missing /etc/os-release)"
+    exit 1
+fi
 
-CHOICE=$?
+echo "Detected OS: $DISTRO_ID"
 
-case $CHOICE in
-    0)
+case "$DISTRO_ID" in
+    arch|artix)
         bash -c "curl -fsSL https://raw.githubusercontent.com/ScoobyChan/Linux-Scripts/refs/heads/main/workstation-arch.sh | sh"
         ;;
-    1)
+
+    debian|ubuntu|linuxmint|pop|zorin)
         bash -c "curl -fsSL https://raw.githubusercontent.com/ScoobyChan/Linux-Scripts/refs/heads/main/workstation-deb.sh | sh"
         ;;
-    2)
+
+    opensuse*|suse|sles)
         bash -c "curl -fsSL https://raw.githubusercontent.com/ScoobyChan/Linux-Scripts/refs/heads/main/workstation-opensuse.sh | sh"
         ;;
-    4)
+
+    rhel|centos|rocky|almalinux|fedora)
         bash -c "curl -fsSL https://raw.githubusercontent.com/ScoobyChan/Linux-Scripts/refs/heads/main/workstation-rhel.sh | sh"
         ;;
-    5)
-        exit 0
+
+    *)
+        echo "Unsupported or unknown distro: $DISTRO_ID"
+        echo "ID_LIKE: $DISTRO_LIKE"
+        exit 1
         ;;
 esac
