0d51f73a951953efe1b0e4bf12e0bbcbd061cd22
ScoobyChan <adam343fay@gmail.com>
2026-05-11 14:44:49 +1200

    Update user-setup.sh

--- user-setup.sh
+++ user-setup.sh
@@ -32,5 +32,9 @@ curl -fsSL https://raw.githubusercontent.com/ScoobyChan/Linux-Scripts/refs/heads
 curl -fsSL https://raw.githubusercontent.com/ScoobyChan/Linux-Scripts/refs/heads/main/bash_profile.sh -o ~/.bash_profile
 curl -fsSL https://raw.githubusercontent.com/ScoobyChan/Linux-Scripts/refs/heads/main/bash_aliasrc.sh -o ~/.bash_aliasrc
 
+dos2unix ~/.bashrc
+dos2unix ~/.bash_profile
+dos2unix ~/.bash_aliasrc
+
 source ~/.bashrc
 source ~/.bash_profile
