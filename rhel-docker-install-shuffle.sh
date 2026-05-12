a3102c36fdd47cea5f67d7e12dca1464c0df0963
Adam Fay <adam.fay@vuw.ac.nz>
2026-05-12 20:38:33 +1200

    fixed

--- rhel-docker-install-shuffle.sh
+++ rhel-docker-install-shuffle.sh
@@ -6,12 +6,7 @@ sudo dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker
 sudo dnf -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 useradd -r svc_shuffle -m -d /usr/shuffle -s /bin/bash -c "Shuffle Service account"
-su -c "git clone https://github.com/Shuffle/Shuffle
-cd Shuffle" svc_shuffle
+su -c "cd /usr/shuffle && git clone https://github.com/Shuffle/Shuffle && chown -R svc_shuffle:svc_shuffle /usr/shuffle/ && cd Shuffle" - svc_shuffle
 
-su -c "
-sudo swapoff -a
-sudo sysctl -w vm.max_map_count=262144
-sudo usermod -aG docker svc_shuffle
-" root
+su -c "sudo swapoff -a && sudo sysctl -w vm.max_map_count=262144 && sudo usermod -aG docker svc_shuffle" - root
 docker compose up -d
