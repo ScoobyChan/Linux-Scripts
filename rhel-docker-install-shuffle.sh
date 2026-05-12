cab9f0a6ff91b691d5db4f06fbb8d8d4dbd349d5
ScoobyChan <adam343fay@gmail.com>
2026-05-12 20:29:08 +1200

    Update rhel-docker-install-shuffle.sh

--- rhel-docker-install-shuffle.sh
+++ rhel-docker-install-shuffle.sh
@@ -2,8 +2,8 @@
 # https://github.com/Shuffle/Shuffle/blob/main/.github/install-guide.md
 
 sudo dnf -y install dnf-plugins-core
-sudo dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
-sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+sudo dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo -y
+sudo dnf -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 useradd -r svc_shuffle -m -d /usr/shuffle -s /bin/bash -c "Shuffle Service account"
 su -c "git clone https://github.com/Shuffle/Shuffle
